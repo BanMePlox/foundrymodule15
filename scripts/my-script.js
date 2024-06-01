c3603c81697a09e4eb472d8d5222fa428d961753
Pedro Jiménez <pedrojimenezlujan1@gmail.com>
2024-06-01 03:16:46 +0200

    Add files via upload

---
+++ scripts/my-script.js
@@ -0,0 +1,15 @@
+// Adds a new "plasma" damage type that will be selectable as a new type of damage for weapons and a new type of resistance for actors.
+Hooks.once("init", () => {
+    CONFIG.DND5E.damageTypes.chakra = {
+      label: "Chakra",
+      isPhysical: true
+    };
+    CONFIG.DND5E.damageTypes.earth = {
+        label: "Earth",
+        isPhysical: true
+      };
+      CONFIG.DND5E.damageTypes.wind = {
+        label: "Wind",
+        isPhysical: true
+      };
+  });
